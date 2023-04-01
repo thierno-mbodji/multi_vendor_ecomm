ae11ddb5e23ee9bec49e60fbbfc63868ff8b5e15
thierno mbodji <thiernosalif.mbodji@uvs.edu.sn>
2023-04-01 18:02:33 +0200

    refactor page dashboard admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -82,7 +82,7 @@
         </div>
     </div><!--end row-->
 
-<div class="row">
+{{-- <div class="row">
     <div class="col-12 col-lg-8 col-xl-8 d-flex">
        <div class="card radius-10 w-100">
         <div class="card-body">
@@ -166,10 +166,10 @@
           </div>
         </div>
     </div>
-   </div><!--End Row-->
+   </div><!--End Row--> --}}
 
 
-   <div class="row row-cols-1 row-cols-lg-3">
+   {{-- <div class="row row-cols-1 row-cols-lg-3">
     <div class="col">
        <div class="card radius-10">
          <div class="card-body">
@@ -218,10 +218,10 @@
          </div>
        </div>
      </div>
-    </div><!--End Row-->
+    </div><!--End Row--> --}}
 
 
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-12 col-lg-12 col-xl-6">
           <div class="card radius-10">
             <div class="card-body">
@@ -345,9 +345,9 @@
              </div>
            </div>
         </div>
-     </div><!--End Row-->
+     </div><!--End Row--> --}}
 
-     <div class="row">
+    {{--  <div class="row">
         <div class="col-12 col-lg-6 col-xl-4 d-flex">
           <div class="card radius-10 overflow-hidden w-100">
              <div class="card-body">
@@ -462,7 +462,7 @@
               </ul>
            </div>
         </div>
-      </div><!--End Row-->
+      </div><!--End Row--> --}}
 
 
       <div class="card radius-10">
