d338f1d0b8b190c09a58eb5c9f27453ac5270788
thierno mbodji <thiernosalif.mbodji@uvs.edu.sn>
2023-04-01 17:32:51 +0200

    page login admin

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 
-<head>  {{ asset('adminBack/') }}
+<head>
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
