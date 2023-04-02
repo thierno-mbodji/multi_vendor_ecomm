2545ab2f555d13cbd93f164f53e49f393d73f4c0
thierno mbodji <thiernosalif.mbodji@uvs.edu.sn>
2023-04-02 02:35:27 +0200

    update profile admin with insertion image

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,6 +29,37 @@ class AdminController extends Controller
 
         return redirect('/admin/login');
     }
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+    }
+
+    public function AdminProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+           /*  @unlink(public_path('upload/admin_images/'.$data->photo)); */
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
+    } // End Mehtod
 
 }
 
