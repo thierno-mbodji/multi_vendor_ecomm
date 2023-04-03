0934dc30a9347121bdf81e70c6fa5ea221f6d3d6
thierno mbodji <thiernosalif.mbodji@uvs.edu.sn>
2023-04-03 02:54:40 +0200

    template frontend loaded

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
+
 <head>
     <meta charset="utf-8" />
-    <title>Easy Shop Online Store </title>
+    <title>Easy shop online store</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -13,79 +14,38 @@
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/imgs/theme/favicon.svg') }}" />
     <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/plugins/animate.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/main.css?v=5.3') }}" />
 </head>
 
 <body>
+    <!-- Modal -->
 
-     @include('frontend.body.header')
+    <!-- Quick view -->
+    @include('frontend.body.quickview')
+    <!-- Header  -->
+    @include('frontend.body.header')
+    <!--End header-->
 
-    <main class="main pages">
-        <div class="page-header breadcrumb-wrap">
-            <div class="container">
-                <div class="breadcrumb">
-                    <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
-                    <span></span>  My Account
-                </div>
-            </div>
-        </div>
-        <div class="page-content pt-150 pb-150">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
-                        <div class="row">
-                            <div class="col-lg-6 pr-30 d-none d-lg-block">
-                                <img class="border-radius-15" src="{{ asset('frontend/assets/imgs/page/login-1.png') }}" alt="" />
-                            </div>
-<div class="col-lg-6 col-md-8">
-    <div class="login_wrap widget-taber-content background-white">
-        <div class="padding_eight_all bg-white">
-            <div class="heading_s1">
-                <h1 class="mb-5">Login</h1>
-                <p class="mb-30">Don't have an account? <a href="page-register.html">Create here</a></p>
-            </div>
 
-            <!-- Password -->
 
-           <form method="POST" action="{{ route('login') }}">
-            @csrf
 
-            <!-- Remember Me -->
 
-                <div class="form-group">
-                    <input type="email" id="email"  required="" name="email" placeholder="Username or Email *" />
-                </div>
-                <div class="form-group">
-                    <input required="" id="password" type="password" name="password" placeholder="Your password *" />
-                </div>
 
-                <div class="login_footer form-group mb-50">
-                    <div class="chek-form">
-                        <div class="custome-checkbox">
-                            <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox1" value="" />
-                            <label class="form-check-label" for="exampleCheckbox1"><span>Remember me</span></label>
-                        </div>
-                    </div>
-                    <a class="text-muted" href="{{ route('password.request') }}">Forgot password?</a>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-heading btn-block hover-up" name="login">Log in</button>
-                </div>
-            </form>
-        </div>
-    </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+
+    <main class="main">
+        @yield('main')
+
     </main>
 
-      @include('frontend.body.footer')
 
 
 
+
+
+
+    @include('frontend.body.footer')
     <!-- Preloader Start -->
     <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
@@ -94,11 +54,10 @@
                     <img src="{{ asset('frontend/assets/imgs/theme/loading.gif') }}" alt="" />
                 </div>
             </div>
-
         </div>
     </div>
     <!-- Vendor JS-->
-   <script src="{{ asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
     <script src="{{ asset('frontend/assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
