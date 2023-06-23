b68b48a3777399e68963873d3850f81488dc6001
thierno mbodji <thiernosalif.mbodji@uvs.edu.sn>
2023-06-23 17:18:04 +0200

    delete product with multi image

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -251,4 +251,25 @@ public function ProductInactive($id){
 
 }// End Method
 
+public function ProductDelete($id){
+
+    $product = Product::findOrFail($id);
+    unlink($product->product_thambnail);
+    Product::findOrFail($id)->delete();
+
+    $imges = MultiImg::where('product_id',$id)->get();
+    foreach($imges as $img){
+        unlink($img->photo_name);
+        MultiImg::where('product_id',$id)->delete();
+    }
+
+    $notification = array(
+        'message' => 'Product Deleted Successfully',
+        'alert-type' => 'success'
+    );
+
+    return redirect()->back()->with($notification);
+
+}// End Method
+
 }
